9fb7cc0f2b35288a597a1e9ab0f37774fca150b2
Miles Spence <37909502+milesspence@users.noreply.github.com>
2021-01-14 23:32:07 -0500

    Update SalesAssociate.java

--- SalesAssociate.java
+++ SalesAssociate.java
@@ -1,20 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project3gui;
 
 /**
  *
- * @author KinkyKat
+ * @author Miles Spence
  */
 public class SalesAssociate extends Login {
     
     public SalesAssociate(String fName, String lName, String email, String username, String password) {
         super(username, password);
     }
-    
-    
-    
+   
 }
