f5c87bd20a17482d92013deecfecb8060c7c2af0
Miles Spence <37909502+milesspence@users.noreply.github.com>
2021-01-14 23:31:40 -0500

    Update Project3gui.java

--- Project3gui.java
+++ Project3gui.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project3gui;
 
 import javafx.application.Application;
@@ -17,7 +12,7 @@ import static project3gui.ProcessCommands.writeFile;
 
 /**
  *
- * @author KinkyKat
+ * @author Miles Spence
  */
 public class Project3gui extends Application {
     
