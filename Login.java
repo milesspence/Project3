b8ffdfc48eb1d4b7180481522661637e7704a454
Miles Spence <37909502+milesspence@users.noreply.github.com>
2021-01-14 23:28:50 -0500

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project3gui;
 
 import java.util.ArrayList;
 
 /**
  *
- * @author KinkyKat
+ * @author Miles Spence
  */
 public class Login implements Interface {
 
